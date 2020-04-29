7d43fd632a67676aa21fec37c5f692855afa5624
gabrielerocha22 <gabrielerocha17@outlook.com>
2020-04-29 14:29:17 -0300

    teste

---
+++ gabi.py
@@ -0,0 +1,14 @@
+x=int(input())
+if 2 < x < 1000:
+    print("1.{}={}".format(x,(1*x)))
+    print("2.{}={}".format(x,(2*x)))
+    print("3.{}={}".format(x,(3*x)))
+    print("4.{}={}".format(x,(4*x)))
+    print("5.{}={}".format(x,(5*x)))
+    print("6.{}={}".format(x,(6*x)))
+    print("7.{}={}".format(x,(7*x)))
+    print("8.{}={}".format(x,(8*x)))
+    print("9.{}={}".format(x,(9*x)))
+    print("10.{}={}".format(x,(10*x)))
+else:
+	print( "error")
